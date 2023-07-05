be09d88935f5a127083d9e90cb6d70ef8a168498
DanieJohnson1990 <daniejohnson1990@gmail.com>
2023-07-05 15:31:07 +0200

    wrote JS logic file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function time(){
+    const clock = ()=>{
+        const time = new Date()
+
+        document.getElementById("display").innerHTML = `${time.getHours()} : ${time.getMinutes()} : ${time.getSeconds()}`
+    }
+    setInterval(clock,1000)
+}
